a092a36b61f84f1dd550d50db43bd5fddf401cb9
Devlynn-E <141595269+Devlynn-E@users.noreply.github.com>
2024-08-09 13:24:40 +1200

    num checker added to base, video 8 done

--- 04_age_checker.py
+++ 04_age_checker.py
@@ -1,34 +1,5 @@
 # functions
 
-# checks if a response was yes or no
-def yes_no(question):
-
-    while True:
-        response = input(question).lower()
-
-        if response == "yes" or response == "y":
-            return "yes"
-
-        elif response == "no" or response == "n":
-            return "no"
-
-        else:
-            print("please enter yes or no")
-
-
-# checks that an input was not left blank
-def not_blank(question):
-
-    while True:
-        response = input(question)
-
-        if response == "":
-            print("This field must be filled")
-
-        else:
-            return response
-
-
 # checks that the user entered a number within given parameters, or an exit code
 def num_check(question, low=12, high=120, exit_code="x"):
 
@@ -63,20 +34,11 @@ def num_check(question, low=12, high=120, exit_code="x"):
 
 
 # main
-
-# set default settings
-max_tickets = 3
 tickets_sold = 0
+max_tickets = 10
 
-# asks if user wants instructions
-want_instructions = yes_no("Do you want to see the instructions? ")
-
-if want_instructions == "yes":
-    print("\ninstructions")
-
-# loop to sell
 while tickets_sold < max_tickets:
-    name = not_blank("What is your name? ('x' to quit) ")
+    name = input("\nEnter your name / x to quit: ")
 
     if name == "x":
         break
@@ -88,9 +50,10 @@ while tickets_sold < max_tickets:
 
     tickets_sold += 1
 
-# output sold tickets
 if tickets_sold == max_tickets:
-    print("you sold all of your tickets")
+    print("\nyou sold all the tickets")
 
 else:
-    print(f"You sold {tickets_sold} ticket/s. you still have {max_tickets - tickets_sold} ticket/s to sell")
+    print(f"\nyou sold {tickets_sold} ticket/s you still have to sell {max_tickets - tickets_sold} ticket/s")
+
+print("\ndone")
